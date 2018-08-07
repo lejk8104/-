724b96526e8e16653243c954e35a769eb6eb0598
Mr.lee <41908895+lejk8104@users.noreply.github.com>
2018-08-07 23:38:02 +0900

    무한루프 계산기
    계산기 설계개요
    1. 스캐너 클래스를 이용. 계산값과 사칙연산을 입력받아 계산.
    2. for문을 이용하여 무한히 입력받고 연산하는 계산기 제작
    3. 사칙연산후 입력값을 100으로 치환시켜 무한히 계산
    (※계산값 또는 사칙연산 입력오류시 continue 부문을 이용하여 오류값만 재수정.
    4. 나눗셈시 음값을 입력시 값을 재입력하도록 유도

---
+++ Go_Calculator.java
@@ -0,0 +1,76 @@
+	import java.util.Scanner;
+public class Go_Calculator {
+	public static void main(String[] args)
+	{
+		String message ="";
+		int a = 100, b = 100; //입력받을 두 숫자(계산이 끝날 경우 0으로 초기화)
+		int result = 0; //결과값
+		double stopper = 0;
+		
+		
+		
+		for(;;)
+		{
+			Scanner scan = new Scanner(System.in);
+			
+			if (a == 100) // 초기화된 값에 새로운 값을 입력받음
+			{
+				Scanner sc = new Scanner(System.in);
+				System.out.print("첫번째 숫자를 입력하시오 : "); //계산할 첫번째 숫자를 입력받음
+				a = sc.nextInt();
+			}
+			
+			if (b == 100) //초기화된 값에 새로운 값을 입력받음
+			{
+				Scanner sc = new Scanner(System.in);
+				System.out.print("두번째 숫자를 입력하시오 : "); //계산할 두번째 숫자를 입력받음
+				b = sc.nextInt();
+			}
+			System.out.print("사칙연산 문자를 입력하시오 : ");
+			message = scan.nextLine(); //사칙연산 입력받음 +, *, /, -
+			
+			if(message.equals("+")) //메세지가 +라면
+			{
+				result=a+b;
+				System.out.println("값 :" +result); //result에 a+b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("-")) //메세지가 -라면
+			{
+				result = a-b;
+				System.out.println("값 :" +result); //result에 a-b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("*")) //메세지가 *라면
+			{
+				result = a*b;
+				System.out.println("값 :" +result); //result에 a*b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("/"))
+			{
+				if(((a<=0)||(b<=0)))  // 계산 할 숫자값 입력  오류
+				{
+					System.out.println("나눌수 없습니다. 숫자를 재입력하시오"); //계산 할 숫자를 재입력
+					continue;
+				}
+				else
+				{
+					result = a/b; //메세지가 / 라면
+					System.out.println("값 : " +result); //result에 a/b라고 출력함
+					//a = 100; b= 100;        //값을 100으로 치환함
+				}	
+			}
+			else 
+			{
+				System.out.println("사칙연산을 잘못 입력하였습니다.");  // 사칙연산 입력 오류
+				System.out.print("사칙연산 문자를 입력하시오 : ");                      // 사칙연산 재입력
+				message = scan.nextLine(); 
+				continue;
+			}
+			
+			}
+			
+			
+}
+}
