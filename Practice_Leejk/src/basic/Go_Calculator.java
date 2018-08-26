37669374f2bf22db7c2a7f7943cccb415912180e
Fhwang0926 <hdh0926@naver.com>
2018-08-26 21:00:22 +0900

    all form change ad create project

---
+++ Practice_Leejk/src/basic/Go_Calculator.java
@@ -0,0 +1,73 @@
+package basic;
+import java.util.Scanner;
+
+public class Go_Calculator {
+	public static void main(String[] args)
+	{
+		String message ="";
+		int a = 100, b = 100; //입력받을 두 숫자(계산이 끝날 경우 0으로 초기화)
+		int result = 0; //결과값
+		@SuppressWarnings("resource") // 컴파일시 에러 출력 안함
+		Scanner sc = new Scanner(System.in);
+		//double stopper = 0; //불필요
+		
+		
+		for(;;)
+		{
+			//Scanner scan = new Scanner(System.in); Scanner는 한번만 선언하면 됌 
+			
+			if (a == 100) // 초기화된 값에 새로운 값을 입력받음
+			{
+				System.out.print("첫번째 숫자를 입력하시오 : "); //계산할 첫번째 숫자를 입력받음
+				a = sc.nextInt();
+			}
+			
+			if (b == 100) //초기화된 값에 새로운 값을 입력받음
+			{				
+				System.out.print("두번째 숫자를 입력하시오 : "); //계산할 두번째 숫자를 입력받음
+				b = sc.nextInt();
+			}
+			System.out.print("사칙연산 문자를 입력하시오 : ");
+			message = sc.nextLine(); //사칙연산 입력받음 +, *, /, -
+			
+			if(message.equals("+")) //메세지가 +라면
+			{
+				result=a+b;
+				System.out.println("값 :" +result); //result에 a+b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("-")) //메세지가 -라면
+			{
+				result = a-b;
+				System.out.println("값 :" +result); //result에 a-b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("*")) //메세지가 *라면
+			{
+				result = a*b;
+				System.out.println("값 :" +result); //result에 a*b라고 출력함
+				a = 100; b= 100;                       //값을 100으로 치환함
+			}
+			else if(message.equals("/"))
+			{
+				if(((a<=0)||(b<=0)))  // 계산 할 숫자값 입력  오류
+				{
+					System.out.println("나눌수 없습니다. 숫자를 재입력하시오"); //계산 할 숫자를 재입력
+					a=100; b=100;
+					continue;
+				}
+				else
+				{
+					result = a/b; //메세지가 / 라면
+					System.out.println("값 : " +result); //result에 a/b라고 출력함
+					a = 100; b= 100;        //값을 100으로 치환함
+				}	
+			}
+			else 
+			{
+				System.out.println("사칙연산을 잘못 입력하였습니다.");  // 사칙연산 입력 오류 
+				continue;
+			}
+		}
+	}
+}
